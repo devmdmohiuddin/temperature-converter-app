a4ad91fe91dab8b684b7dee2af0c5bfc4cbb51b6
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-01-24 13:19:28 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Temperature Converter app
+
+When we will give a value of temperature in input field it automatically will be converted. The value will be celsius or fahrenheit.
+
+
---
+++ src/components/TemperatureInput.jsx
@@ -0,0 +1,20 @@
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+}
+
+const TemperatureInput = ({ temperature, scale, onTemperatureChange }) => {
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}</legend>
+        <input
+          type="text"
+          value={temperature}
+          onChange={(e) => onTemperatureChange(e, scale)}
+        />
+      </fieldset>
+    );
+};
+
+export default TemperatureInput;
